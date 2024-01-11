079b3cfebe304778cb3bccff450c9934ad12dfeb
sakutyandesu <131092105+sakutyandesu@users.noreply.github.com>
2024-01-11 10:42:54 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -1,5 +1,1 @@
-function sayhllo66(){alert('正解,'+who66+''!');}
-function sayhllo66(){alert('はずれ,'+who66+''!');}
-function kimetu66(){who66='鬼滅の刃';}
-function vaiorextuto66(){who66='ヴァイオレット・エヴァーガーデン';}
-function kusuriya66(){who66='薬屋のひとりごと';}
+
